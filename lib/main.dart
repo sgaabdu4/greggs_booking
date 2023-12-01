b59b48b9d0e09e9b310932f237384924521d386b
sgaabdu4 <abdulgafoorabid@gmail.com>
2023-12-01 14:07:11 +0000

    added future provider before homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:greggs_booking/api_classes.dart';
 import 'package:greggs_booking/homepage.dart';
+import 'package:greggs_booking/providers.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<GreggsAPI> greggsAPI = ref.watch(greggsAPIProvider);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.montserrat().fontFamily,
-        colorScheme: ColorScheme.fromSeed(
-            background: Color.fromARGB(255, 241, 250, 255),
-            seedColor: const Color.fromARGB(255, 0, 110, 176)),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-            style: OutlinedButton.styleFrom(
-          foregroundColor: const Color.fromARGB(255, 239, 139, 3),
-          side: const BorderSide(color: Color.fromARGB(255, 239, 139, 3)),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-        )),
-        filledButtonTheme: FilledButtonThemeData(
-            style: FilledButton.styleFrom(
-          disabledBackgroundColor: const Color.fromARGB(255, 239, 139, 3),
-          disabledForegroundColor: Colors.white,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-        )),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          fontFamily: GoogleFonts.montserrat().fontFamily,
+          colorScheme: ColorScheme.fromSeed(
+              background: Color.fromARGB(255, 241, 250, 255),
+              seedColor: const Color.fromARGB(255, 0, 110, 176)),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+              style: OutlinedButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 239, 139, 3),
+            side: const BorderSide(color: Color.fromARGB(255, 239, 139, 3)),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          )),
+          filledButtonTheme: FilledButtonThemeData(
+              style: FilledButton.styleFrom(
+            disabledBackgroundColor: const Color.fromARGB(255, 239, 139, 3),
+            disabledForegroundColor: Colors.white,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          )),
+          useMaterial3: true,
+        ),
+        home: greggsAPI.when(
+          loading: () =>
+              const Scaffold(body: Center(child: CircularProgressIndicator())),
+          error: (err, stack) => Text('Error: $err'),
+          data: (data) => const MyHomePage(),
+        ));
   }
 }
